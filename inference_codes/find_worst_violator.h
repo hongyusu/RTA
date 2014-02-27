05d0fea6b7a3ad551489ba342a806c8f77af2594
hongyusu <hongyu.su.uh@gmail.com>
2014-02-27 17:44:34 +0200

    minor

--- inference_codes/find_worst_violator.h
+++ inference_codes/find_worst_violator.h
@@ -10,14 +10,6 @@ struct type_element_list
    double val;
    struct type_element_list * next;
 };
-//         
-// struct type_element_list
-// {
-//     double id;
-//     double score;
-//     struct type_element_list * next_element;
-// } ;
-
 
 void printm(double * M, mint nrow, mint ncol);
 void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] );
