184623e58dcba3024097a27c6e0d641979811506
hongyusu <hongyu.su.uh@gmail.com>
2014-05-18 22:46:20 +0300

    minor

--- inference_codes/auto_run_RSTA.py
+++ inference_codes/auto_run_RSTA.py
@@ -27,13 +27,11 @@ def run():
   #cluster = ['dave']
   jobs=[]
   n=0
-  is_main_run=1.5
+  is_main_run=2
 
   #filenames=['emotions','yeast','scene','enron','cal500','fp','cancer','medical','toy10','toy50','toy100'] 
   filenames=['toy10','emotions','yeast','medical','scene','enron','toy50','ArD10','ArD15','ArD20','ArD30']#,'fp','cancer','cal500']
-  filenames=['ArD10','ArD15','ArD20','ArD30']
-  filenames=['toy10','emotions','yeast','medical','scene','enron']#'toy50','ArD10','ArD15','ArD20','ArD30']#,'fp','cancer','cal500']
-  filenames=['toy50']
+  filenames=['ArD10','ArD15','ArD20','ArD30','toy10','toy50','emotions','yeast','medical','scene','enron']#'toy50','ArD10','ArD15','ArD20','ArD30']#,'fp','cancer','cal500']
   n=0
   for kth_fold in ['1']:#,'2','3','4','5']:
     for filename in filenames:
