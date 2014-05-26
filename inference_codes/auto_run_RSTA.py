345e87117bac2f17d4fbb436820ec1e4bb84f45b
hongyusu <hongyu.su.uh@gmail.com>
2014-05-26 12:43:45 +0300

    worker class

--- inference_codes/auto_run_RSTA.py
+++ inference_codes/auto_run_RSTA.py
@@ -1,4 +1,11 @@
 
+
+
+import math
+import re
+import Queue
+from threading import ThreadError
+from threading import Thread
 import os
 import sys
 import commands
@@ -10,31 +17,59 @@ import logging
 logging.basicConfig(format='%(asctime)s %(filename)s %(funcName)s %(levelname)s:%(message)s', level=logging.INFO)
 
 
+job_queue = Queue.Queue()
 
-def singleRSTA(filename,graph_type,t,node,kth_fold,l_norm,kappa):
+
+# Worker class
+# job is a tuple of parameters
+class Worker(Thread):
+  def __init__(self, job_queue, node):
+    Thread.__init__(self)
+    self.job_queue  = job_queue
+    self.node = node
+    pass # def
+ def run(self):
+    all_done = 0
+    while not all_done:
+      try:
+        job = self.job_queue.get(0)
+        time.sleep(random.randint(5000,6000) / 1000.0)  # sleep random time
+        single_thread(self.node, job)
+      except Queue.Empty:
+        all_done = 1
+      pass
+    pass # def
+  pass # class
+
+
+def singleRSTA(node, job):
+  (n,filename,graph_type,t,kth_fold,l_norm,kappa) = job
   try:
-    with open("../outputs/%s_%s_%s_f%s_l%s_k%s_RSTAs.log" % (filename,graph_type,t,kth_fold,l_norm,kappa)): pass
-    logging.info('\t--< (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
+    if os.path.isfile("../outputs/%s_%s_%s_f%s_l%s_k%s_RSTAs.log" % (filename,graph_type,t,kth_fold,l_norm,kappa)):
+      logging.info('\t--< (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
+    else:
+      logging.info('\t--> (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
+      print(""" ssh -o StrictHostKeyChecking=no %s 'cd /cs/taatto/group/urenzyme/workspace/colt2014/experiments/random_spanning_tree_approximation/inference_codes/; rm -rf /var/tmp/.matlab; export OMP_NUM_THREADS=32; nohup matlab -nodisplay -r "run_RSTA '%s' '%s' '%s' '0' '%s' '%s' '%s' " > /var/tmp/tmp_%s_%s_%s_f%s_l%s_k%s_RSTAs' """ % (node,filename,graph_type,t,kth_fold,l_norm,kappa,filename,graph_type,t,kth_fold,l_norm,kappa) )
+      logging.info('\t--| (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
+      time.sleep(5)
   except:
-    logging.info('\t--> (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
-    os.system(""" ssh -o StrictHostKeyChecking=no %s 'cd /cs/taatto/group/urenzyme/workspace/colt2014/experiments/random_spanning_tree_approximation/inference_codes/; rm -rf /var/tmp/.matlab; export OMP_NUM_THREADS=32; nohup matlab -nodisplay -r "run_RSTA '%s' '%s' '%s' '0' '%s' '%s' '%s' " > /var/tmp/tmp_%s_%s_%s_f%s_l%s_k%s_RSTAs' """ % (node,filename,graph_type,t,kth_fold,l_norm,kappa,filename,graph_type,t,kth_fold,l_norm,kappa) )
-    logging.info('\t--| (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
+    job_queue.put((job))
+    logging.info('\t--X (node)%s,(f)%s,(type)%s,(t)%s,(f)%s,(l)%s,(k)%s' %( node,filename,graph_type,t,kth_fold,l_norm,kappa))
     time.sleep(5)
-  pass
+  pass # def
+
 
 def run():
   cluster = get_free_nodes()[0]
   #cluster = ['dave']
   jobs=[]
   n=0
-  is_main_run=5
+  is_main_run_factor=5
 
-  #filenames=['emotions','yeast','scene','enron','cal500','fp','cancer','medical','toy10','toy50','toy100'] 
-  filenames=['toy10','emotions','yeast','medical','scene','enron','toy50','ArD10','ArD15','ArD20','ArD30']#,'fp','cancer','cal500']
-  filenames=['ArD10','ArD15','ArD20','ArD30','toy10','toy50','emotions','yeast','medical','scene','enron']#'toy50','ArD10','ArD15','ArD20','ArD30']#,'fp','cancer','cal500']
-  filenames=['cancer','ArD20','ArD30','toy10','toy50','emotions','yeast','medical','scene','enron','cal500']#,'fp','cancer']
+  filenames=['cancer','ArD20','ArD30','toy10','toy50','emotions','yeast','medical','scene','enron','cal500']#,'fp']
   n=0
-  for kth_fold in ['1','2']:#,'3','4','5']:
+  # generate jobs
+  for kth_fold in ['1']:#,'2']:#,'3','4','5']:
     for filename in filenames:
       graph_type = 'tree'
       for kappa in ['2','8','16','20']:
@@ -47,25 +82,32 @@ def run():
               with open("../outputs/%s_%s_%s_f%s_l%s_k%s_RSTAs.log" % (filename,graph_type,para_t,kth_fold,l_norm,kappa)): pass
               continue
             except:
-              node=cluster[n%len(cluster)]
-              n+=1
-              p=multiprocessing.Process(target=singleRSTA, args=(filename,graph_type,para_t,node,kth_fold,l_norm,kappa,))
-              jobs.append(p)
-              p.start()
-              time.sleep(30) # fold
+              n=n+1
+              job_queue.put((n,filename,graph_type,para_t,kth_fold,l_norm,kappa))
             pass
-          time.sleep(10*is_main_run) # l norm
           pass
-      time.sleep(10*is_main_run) # file
       pass
-    time.sleep(60*is_main_run) # t
     pass
-  for job in jobs:
-    job.join()
+  # running jobs
+  job_size = job_queue.qsize()
+  for i in len(cluster):
+    t = Worker(job_queue, cluster[t])
+    time.sleep(1)
+    try:
+      t.start()
+      threads.append(t)
+    except ThreadError:
+      logging.warning("\t\tError: thread error caught!")
     pass
+  for t in threads:
+    t.join()
+    pass
+  pass # def
 
 
 
-run()
+if __name__ == "__main__":
+  run()
+  pass
 
 
