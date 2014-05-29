f4e4cebb0abbbfe3112b0edf56dc619716025d60
hongyusu <hongyu.su.uh@gmail.com>
2014-05-29 22:40:47 +0300

    minor

--- inference_codes/auto_profile_RSTA.py
+++ inference_codes/auto_profile_RSTA.py
@@ -84,10 +84,9 @@ def run():
   for kth_fold in ['1']:#,'2','3','4','5']:
     for filename in filenames:
       graph_type = 'tree'
-      #for kappa in ['2','8','16','20','32']:
-      for kappa in ['4']:
+      for kappa in ['2','4','8','16','20','32']:
         for l_norm in ['2']:
-          for t in range(0,41,10):
+          for t in [5]:#range(0,41,10):
             if t==0:
               t=1
             para_t="%d" % (t)
